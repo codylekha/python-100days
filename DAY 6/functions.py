4466796d731a6469314634df08e9992455cd887c
codylekha <81819243+codylekha@users.noreply.github.com>
2021-05-01 23:18:25 +0530

    DAY 6

---
+++ DAY 6/functions.py
@@ -0,0 +1,32 @@
+#builtin functions
+#print() len() range() input() int()
+
+#user function
+def my_function():
+    #do this
+    #after do this
+    #finally do this
+    print("hello")
+    
+#call this function to use
+my_function()    
+
+
+# for loop
+# for items in list_of_items:
+#     #do something
+
+# while Loop 
+# while something_is_true:
+        #do This
+        # do this    
+
+
+# when to use forloop
+# when to iterate something do with each thing
+
+# when to use whileloop
+# when do  carry out some sort of condtions
+# whileloop is dangerous that forloop 
+# for runs upto range but while not it is infiniteloop
+
